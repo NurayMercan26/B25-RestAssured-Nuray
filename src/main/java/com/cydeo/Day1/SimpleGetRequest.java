f42d88a05f4d20c5b7aa086db778bc089f376c0b
NurayMercan26 <nuraymercan26@gmail.com>
2022-11-08 15:29:04 +0300

    Reguest3

--- src/main/java/com/cydeo/Day1/SimpleGetRequest.java
+++ src/main/java/com/cydeo/Day1/SimpleGetRequest.java
@@ -1,2 +1,26 @@
-package com.cydeo.Day1;public class SimpleGetRequest {
+package com.cydeo.Day1;
+
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
+public class SimpleGetRequest {
+
+    String url = "http://54.175.31.71:8000/api/spartans";
+
+    @Test
+    public void test1(){
+        //send a get request and save response inside the Response object
+        Response response = RestAssured.get(url);
+
+        //print response status code
+        System.out.println(response.statusCode());
+
+        //print response body
+        response.prettyPrint();
+
+    }
+
 }
